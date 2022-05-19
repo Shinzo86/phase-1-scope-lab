cc7b445fada1c3d35a92085b47113bbab2fe1668
Adrian Alberto <adrianalberto1986@gmail.com>
2022-05-18 20:05:36 -0500

    commit tree cf8996d20896c0fb09655889a0bd7004bcce05f4

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+var bestCustomer
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'whatever'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'so what'
+}
